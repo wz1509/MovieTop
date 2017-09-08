eb5d7c18c3caa2e704fdecb2ed9107f7664e8a71
wangzheng1509 <https://github.com/wangzheng1509/mvp_dagger_rxjava_retrofit/tree/master>
2017-09-08 12:13:21 +0800

    内存泄漏检查

--- app/src/main/java/me/jokey/movie/application/MovieApplication.java
+++ app/src/main/java/me/jokey/movie/application/MovieApplication.java
@@ -2,6 +2,8 @@ package me.jokey.movie.application;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by wz on 2017/9/7 15:07.
  * desc:
@@ -12,6 +14,12 @@ public class MovieApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
     }
 
